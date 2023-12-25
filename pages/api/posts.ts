fe8e8606fc4c431b89ed364fe50abec0fb1d2f07
Gamze <gamzeyasar35@icloud.com>
2023-12-25 14:55:35 +0300

    post  step 1

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,4 +1,6 @@
 import { NextApiHandler } from "next";
+import fs from "fs";
+import path from "path";
 
 // Methods
 // GET => when you need to just get or read the data.
@@ -11,11 +13,24 @@ const handler: NextApiHandler = (req, res) => {
   const { method } = req;
 
   switch (method) {
-    case "GET":
-      return res.json({ ok: true });
+    case "GET": {
+      const data = readPostInfo();
+      return res.json({ postInfo: data });
+    }
+
     default:
       return res.status(404).send("Not Found");
   }
+};
+
+const readPostInfo = () => {
+  const dirPathToRead = path.join(process.cwd(), "posts");
+  const dirs = fs.readdirSync(dirPathToRead);
+  dirs.map((filename) => {
+    const filePathToRead = path.join(process.cwd(), "posts/" + filename);
+    const fileContent = fs.readFileSync(filePathToRead, { encoding: "utf-8" });
+    console.log(fileContent);
+  });
 };
 
 export default handler;
