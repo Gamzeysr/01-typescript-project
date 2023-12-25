b96508a40e964fe5fa607ee1d8fac60193088eac
Gamze <gamzeyasar35@icloud.com>
2023-12-25 13:41:41 +0300

    blogCard

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,9 +1,31 @@
 import { NextPage} from 'next';
+import BlogCard from '../component/BlogCard';
 
 interface Props {}
 
-const blogs: NextPage <Props>= () => {
-return <div>blogs</div>;
+const Blogs: NextPage <Props>= () => {
+
+return (
+
+<div className='max-w-3xl mx-auto p-5 space-y-5'>
+<BlogCard 
+title="This is my blog" 
+desc= "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit deserunt ea asperiores, maxime expedita odit debitis veritatis non optio. Animi."/>
+
+<BlogCard 
+title="This is my blog" 
+desc= "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit deserunt ea asperiores, maxime expedita odit debitis veritatis non optio. Animi."/>
+
+
+<BlogCard 
+title="This is my blog" 
+desc= "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit deserunt ea asperiores, maxime expedita odit debitis veritatis non optio. Animi."/>
+
+
+</div>
+)
+
 };
 
-export default blogs;
+export default Blogs;
+
